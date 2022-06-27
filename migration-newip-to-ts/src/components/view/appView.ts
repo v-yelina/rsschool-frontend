213f5b08555c47360ae4fa34537b68e461231f06
v-yelina <heytridvaodin@gmail.com>
2022-06-27 23:14:44 +0200

    feat: add types to appView.js

--- migration-newip-to-ts/src/components/view/appView.ts
+++ migration-newip-to-ts/src/components/view/appView.ts
@@ -1,18 +1,23 @@
 import News from './news/news';
 import Sources from './sources/sources';
+import { SourcesData } from './sources/sourceData.interface';
+import { AllNewsData } from './news/newsData.interface';
 
 export class AppView {
+    news: News;
+    sources: Sources;
+
     constructor() {
         this.news = new News();
         this.sources = new Sources();
     }
 
-    drawNews(data) {
+    drawNews(data: AllNewsData) {
         const values = data?.articles ? data?.articles : [];
         this.news.draw(values);
     }
 
-    drawSources(data) {
+    drawSources(data: SourcesData) {
         const values = data?.sources ? data?.sources : [];
         this.sources.draw(values);
     }
