359546a9cbf4d328faf103339d79608839b94a4d
v-yelina <heytridvaodin@gmail.com>
2022-06-27 23:10:57 +0200

    feat: add types to app.js

--- migration-newip-to-ts/src/components/app/app.ts
+++ migration-newip-to-ts/src/components/app/app.ts
@@ -1,7 +1,10 @@
 import AppController from '../controller/controller';
-import { AppView } from '../view/appView.js';
+import { AppView } from '../view/appView';
 
 class App {
+    controller: AppController;
+    view: AppView;
+
     constructor() {
         this.controller = new AppController();
         this.view = new AppView();
@@ -9,7 +12,7 @@ class App {
 
     start() {
         document
-            .querySelector('.sources')
+            .querySelector('.sources')!
             .addEventListener('click', (e) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
         this.controller.getSources((data) => {
             this.view.drawSources(data);
