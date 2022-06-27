6bfa3bbede9ca66e30b322aa0d3d922e3ef1f33f
v-yelina <heytridvaodin@gmail.com>
2022-06-27 23:15:59 +0200

    feat: add types to controller.js

--- migration-newip-to-ts/src/components/controller/controller.ts
+++ migration-newip-to-ts/src/components/controller/controller.ts
@@ -1,9 +1,9 @@
 import AppLoader from './appLoader';
+import { SourcesData } from '../view/sources/sourceData.interface';
+import { AllNewsData } from '../view/news/newsData.interface';
 
 class AppController extends AppLoader {
-    // @ts-ignore
-
-    getSources(callback) {
+    getSources(callback: (data: SourcesData) => void) {
         super.getResp(
             {
                 endpoint: 'sources',
@@ -12,13 +12,13 @@ class AppController extends AppLoader {
         );
     }
 
-    getNews(e, callback) {
-        let target = e.target;
-        const newsContainer = e.currentTarget;
+    getNews(e: Event, callback: (data: AllNewsData) => void) {
+        let target = e.target as HTMLElement;
+        const newsContainer = e.currentTarget as HTMLElement;
 
         while (target !== newsContainer) {
             if (target.classList.contains('source__item')) {
-                const sourceId = target.getAttribute('data-source-id');
+                const sourceId = target.getAttribute('data-source-id') as string;
                 if (newsContainer.getAttribute('data-source') !== sourceId) {
                     newsContainer.setAttribute('data-source', sourceId);
                     super.getResp(
@@ -33,15 +33,16 @@ class AppController extends AppLoader {
                 }
                 return;
             }
-            target = target.parentNode;
+            target = target.parentNode as HTMLElement;
         }
     }
 
-    chooseFirstCategory(callback) {
-        const newsContainer = document.querySelector('.sources');
-        const firstCategory = document.querySelector('.source__item');
-        console.log(firstCategory);
-        const sourceId = firstCategory.getAttribute('data-source-id');
+    chooseFirstCategory(callback: (data: AllNewsData) => void) {
+        const newsContainer = document.querySelector('.sources') as HTMLElement;
+        const firstCategory = document.querySelector('.source__item') as HTMLElement;
+
+        const sourceId = firstCategory.getAttribute('data-source-id') as string;
+
         if (newsContainer.getAttribute('data-source') !== sourceId) {
             newsContainer.setAttribute('data-source', sourceId);
             super.getResp(
