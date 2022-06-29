bc4a77cad8d9b9bd049af3b0b68462b7eda1bedf
v-yelina <heytridvaodin@gmail.com>
2022-06-29 10:33:51 +0200

    fix: linter errors

--- migration-newip-to-ts/src/components/app/app.ts
+++ migration-newip-to-ts/src/components/app/app.ts
@@ -13,7 +13,7 @@ class App {
     start() {
         document
             .querySelector('.sources')!
-            .addEventListener('click', (e) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
+            .addEventListener('click', (e: Event) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
         this.controller.getSources((data) => {
             this.view.drawSources(data);
             this.controller.chooseFirstCategory((data) => this.view.drawNews(data));
