972efa71f22190f79176b5c6f4172d4c0b794d36
v-yelina <heytridvaodin@gmail.com>
2022-07-04 14:44:51 +0200

    refactor: rename task folder

--- online-shop/webpack.config.js
+++ online-shop/webpack.config.js
@@ -10,13 +10,13 @@ const baseConfig = {
     module: {
         rules: [
             {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
                 test: /\.tsx?$/,
                 use: 'ts-loader',
                 exclude: /node_modules/,
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
             },
         ],
     },
