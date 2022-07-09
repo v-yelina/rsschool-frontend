e4b3a90d1f9fea6ac868c05ad0f505a4caec50b9
v-yelina <heytridvaodin@gmail.com>
2022-07-09 22:46:14 +0200

    feat: add sorting by year and title

--- online-shop/src/components/view/sort/sort.ts
+++ online-shop/src/components/view/sort/sort.ts
@@ -3,36 +3,55 @@ class Sort {
         const sortBy = localStorage.getItem('sortBy');
         switch (sortBy) {
             case 'price-ascending':
-                this.sortByPrice('ascending');
+                this.sort('price', 'ascending');
                 return;
             case 'price-descending':
-                this.sortByPrice('descending');
+                this.sort('price', 'descending');
                 return;
             case 'title-ascending':
+                this.sort('title', 'ascending');
                 return;
             case 'title-descending':
+                this.sort('title', 'descending');
                 return;
             case 'year-ascending':
+                this.sort('year', 'ascending');
                 return;
             case 'year-descending':
+                this.sort('year', 'descending');
                 return;
         }
     }
 
-    private sortByPrice(direction: string) {
+    private sort(filter: string, direction: string) {
         const products = Array.from(document.querySelectorAll('.card__item'));
 
-        if (direction === 'ascending') {
+        const filterClass =
+            filter === 'price' ? '.card__item-price' : filter === 'title' ? '.card__item-title' : '.card__item-year';
+
+        if (direction === 'ascending' && filter === 'title') {
+            products.sort((a, b) => {
+                return (a.querySelector(filterClass)?.innerHTML as string).localeCompare(
+                    b.querySelector(filterClass)?.innerHTML as string
+                );
+            });
+        } else if (direction === 'descending' && filter === 'title') {
+            products.sort((a, b) => {
+                return (b.querySelector(filterClass)?.innerHTML as string).localeCompare(
+                    a.querySelector(filterClass)?.innerHTML as string
+                );
+            });
+        } else if (direction === 'ascending') {
             products.sort(
                 (a, b) =>
-                    parseFloat(a.querySelector('.card__item-price')?.innerHTML as string) -
-                    parseFloat(b.querySelector('.card__item-price')?.innerHTML as string)
+                    parseFloat(a.querySelector(filterClass)?.innerHTML as string) -
+                    parseFloat(b.querySelector(filterClass)?.innerHTML as string)
             );
         } else if (direction === 'descending') {
             products.sort(
                 (a, b) =>
-                    parseFloat(b.querySelector('.card__item-price')?.innerHTML as string) -
-                    parseFloat(a.querySelector('.card__item-price')?.innerHTML as string)
+                    parseFloat(b.querySelector(filterClass)?.innerHTML as string) -
+                    parseFloat(a.querySelector(filterClass)?.innerHTML as string)
             );
         }
         const productsWrapper = document.querySelector('.products') as HTMLDivElement;
