42c7c6aad6b5d146a4d38b71d1a9166cc3aab799
v-yelina <heytridvaodin@gmail.com>
2022-07-14 21:57:04 +0200

    fix: prices for sale products

--- online-shop/src/components/view/card/card.ts
+++ online-shop/src/components/view/card/card.ts
@@ -25,11 +25,12 @@ class Card {
 
                     cardItem.setAttribute('data-filters', `${item.brush}-${item.author.toLowerCase()}-${item.color}`);
                     if (item.onsale && item.salePrice) {
-                        cardItemPrice.classList.add('card__item-price_sale');
+                        cardItemPrice.classList.add('card__item_sale-price');
                         const salePrice = document.createElement('span');
-                        salePrice.classList.add('card__item_sale-price');
-                        salePrice.innerHTML = `${item.salePrice}$`;
-                        cardItemPrice.after(salePrice);
+                        salePrice.classList.add('card__item-price_sale');
+                        salePrice.innerHTML = `${item.price}$`;
+                        cardItemPrice.innerHTML = `${item.salePrice}$`;
+                        cardItemPrice.before(salePrice);
                         const salePercent = document.createElement('span');
                         salePercent.classList.add('card__item_sale-percent');
                         salePercent.innerHTML = `-${Math.round(100 - (item.salePrice * 100) / item.price)}%`;
