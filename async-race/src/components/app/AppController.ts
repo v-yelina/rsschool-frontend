3eeb9df3d97718d4ca81ea880197e5bd3a9e5c69
v-yelina <heytridvaodin@gmail.com>
2022-07-29 08:03:33 +0200

    feat: add header

--- async-race/src/components/app/AppController.ts
+++ async-race/src/components/app/AppController.ts
@@ -1,12 +1,17 @@
 import Footer from '../layout/footer';
+import Header from '../layout/header';
 
 export class AppController {
     footer: Footer;
+    header: Header;
 
     constructor() {
+        this.header = new Header();
         this.footer = new Footer();
     }
+
     public start() {
+        this.header.draw();
         this.footer.draw();
     }
 }
