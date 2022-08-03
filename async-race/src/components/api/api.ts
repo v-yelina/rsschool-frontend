858c7c5199f46f44b740c706df4a49bb9675b5aa
v-yelina <heytridvaodin@gmail.com>
2022-08-03 22:30:02 +0200

    feat: get one car from api

--- async-race/src/components/api/api.ts
+++ async-race/src/components/api/api.ts
@@ -1,12 +1,20 @@
 import { state } from '../app/state';
 import { ICar } from '../car/car.interface';
 import ENV from '../common/config';
+import { IWinner } from '../winners/winners.interface';
 
 export const getCars = async (page = state.page, limit = 7): Promise<{ page: string; count: string; data: ICar[] }> => {
     const response = await fetch(`${ENV.API_ROOT}${ENV.GARAGE}?_limit=${limit}&_page=${page}`);
 
     const data: ICar[] = await response.json();
     return { page: page, count: response.headers.get('X-Total-Count') || '0', data };
+};
+
+export const getOneCar = async (id: string): Promise<ICar> => {
+    const response = await fetch(`${ENV.API_ROOT}${ENV.GARAGE}/${id}`);
+
+    const data: ICar = await response.json();
+    return data;
 };
 
 export const addCars = async (data: Partial<ICar>): Promise<ICar> => {
